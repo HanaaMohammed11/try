347a1df11a59639dadd715bfcff2f4d1e9778f8a
YoussefFathy6 <yousseffathy20@gmail.com>
2024-10-01 17:18:58 +0300

    creating account added

--- src/pages/Dashboard/AdminDashboard.jsx
+++ src/pages/Dashboard/AdminDashboard.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import SideBar from "./SideBar";
 import MatrixForm from "./Componants/Matrix/MatrixForm";
-import  MatrixList  from "./Componants/Matrix/MatrixList";
+import MatrixList from "./Componants/Matrix/MatrixList";
 import EditTheme from "./Componants/EditTheme";
 import AdminUserCard from "./Componants/users/AdminUserCard";
 import AdminUsers from "./Componants/users/AdminUsers";
@@ -17,21 +17,16 @@ function AdminDashboard() {
 
   return (
     <div className="flex flex-row-reverse min-h-screen bg-gray-100">
-
       <div className="w-64">
         <SideBar activeItem={activeItem} onItemClick={handleItemClick} />
       </div>
 
- 
       <div className="flex-grow   ">
         {activeItem === "المصفوفات" && <MatrixList />}
         {activeItem === "تعديل المظهر" && <EditTheme />}
         {activeItem === "الموظفين" && <AdminUsers />}
         {activeItem === "الصلاحيات" && <SubjectList />}
-
-    
       </div>
-     
     </div>
   );
 }
