79f7e5c83c1af66a71d49057f79d1f072c3e312c
YoussefFathy6 <yousseffathy20@gmail.com>
2024-10-03 23:24:16 +0300

    Search updated

--- src/pages/Dashboard/Componants/Addaccunts.jsx
+++ src/pages/Dashboard/Componants/Addaccunts.jsx
@@ -2,10 +2,9 @@ import { Button, Label, Modal, TextInput } from "flowbite-react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import emailjs from "emailjs-com";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import db from "../../../config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import db, { auth } from "../../../config/firebase";
 import { addDoc, collection } from "firebase/firestore";
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import Topbanner from "../../Home/componants/banner/Topbanner";
 import Bottombanner from "../../Home/componants/banner/Bottombanner";
@@ -51,79 +50,6 @@ export default function AddAccounts() {
       );
       const user = userCredential.user;
       console.log("تم تسجيل المستخدم:", user);
-=======
-import React, { useState, useEffect } from 'react';
-import { getDocs, query, where } from 'firebase/firestore';
-
-// Initialize EmailJS
-emailjs.init("vRSobHxRYCwqKML2w"); 
-
-export default function AddAccounts() {
-    const [openModal, setOpenModal] = useState(false);
-    const [error, setError] = useState("");
-    const usersCollection = collection(db, "users");
-    const auth = getAuth(); // Make sure to initialize Firebase Auth correctly
-
-    const initialValues = {
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-    };
-
-    const validationSchema = Yup.object().shape({
-        firstName: Yup.string().required("الاسم الأول مطلوب"),
-        lastName: Yup.string().required("الاسم الأخير مطلوب"),
-        email: Yup.string()
-            .email("البريد الإلكتروني غير صحيح")
-            .required("البريد الإلكتروني مطلوب"),
-        password: Yup.string()
-            .min(6, "يجب أن يكون الرمز السري 6 أحرف على الأقل")
-            .required("الرمز السري مطلوب"),
-    });
-
-    const handleRegister = async (values, { setSubmitting }) => {
-        const { email, password, firstName, lastName } = values;
-
-        try {
-            setError(""); // Clear any previous errors
-            setSubmitting(true); // Indicate form is being submitted
-
-            // Create user with email and password using Firebase Auth
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user;
-            console.log("تم تسجيل المستخدم:", user);
-
-            // Add user to Firestore
-            const docRef = await addDoc(usersCollection, {
-                firstname: firstName,
-                lastname: lastName,
-                email: email,
-                id: user.uid,
-                accountType: "employee",
-                password: password, // Not recommended to store plain passwords in Firestore
-            });
-
-            console.log("User added to Firestore with ID:", docRef.id);
-
-            // Send email using EmailJS
-            await emailjs.send('service_1go7kvh', 'template_wcch0ap', {
-                to_Email: email,
-                from_name: 'CorGov',
-                reply_to: email,
-                User_Email: email,
-                User_password: password, // Placeholder for password in email (use with caution)
-            });
-
-            console.log('تم إرسال البريد الإلكتروني بنجاح!');
-        } catch (error) {
-            console.error("Error during registration:", error);
-            setError("حدث خطأ أثناء تسجيل المستخدم.");
-        } finally {
-            setSubmitting(false); // Set submitting to false once complete
-        }
-    };
->>>>>>> 2052b456d75835656b5b5701fca43ad98f03c651
 
       const docRef = await addDoc(usersCollection, {
         firstname: firstName,
@@ -134,7 +60,6 @@ export default function AddAccounts() {
         password: password,
       });
 
-<<<<<<< HEAD
       const userData = {
         id: user.uid,
         firstname: firstName,
@@ -143,22 +68,6 @@ export default function AddAccounts() {
         accountType: "employee",
         password: password,
       };
-=======
-    useEffect(() => {
-        const fetchEmployees = async () => {
-            try {
-                const q = query(collection(db, 'users'), where('accountType', '==', 'employee'));
-                const querySnapshot = await getDocs(q);
-                const employeeList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-                setEmployees(employeeList);
-            } catch (error) {
-                console.error('Error fetching employees:', error);
-            }
-        };
-
-        fetchEmployees();
-    }, []);
->>>>>>> 2052b456d75835656b5b5701fca43ad98f03c651
 
       if (!email) {
         setError("البريد الإلكتروني الجديد مطلوب");
@@ -242,7 +151,6 @@ export default function AddAccounts() {
         إنشاء حساب مستخدم
       </div>
 
-<<<<<<< HEAD
       <Modal
         show={openModal}
         size="md"
@@ -388,67 +296,4 @@ export default function AddAccounts() {
       </div>
     </div>
   );
-=======
-                                    <div>
-                                        <div className="mb-2 block text-right">
-                                            <Label htmlFor="password" value="الرمز السري" />
-                                        </div>
-                                        <Field
-                                            name="password"
-                                            type="password"
-                                            as={TextInput}
-                                            id="password"
-                                            placeholder="••••••••"
-                                        />
-                                        <ErrorMessage
-                                            name="password"
-                                            component="div"
-                                            className="text-red-500 text-sm"
-                                        />
-                                    </div>
-
-                                    <div className="w-full">
-                                        <Button type="submit" disabled={isSubmitting}>
-                                            {isSubmitting ? "جاري التسجيل..." : "تسجيل"}
-                                        </Button>
-                                    </div>
-                                </div>
-                            </Form>
-                        )}
-                    </Formik>
-                </Modal.Body>
-            </Modal>
-
-            {/* Table displaying employees */}
-            <div className="min-h-screen bg-gray-100 flex flex-col items-center">
-                <div className="w-[900px] h-auto bg-white p-4 rounded-lg shadow-lg mt-10">
-                    <table className="min-w-full text-right border-collapse">
-                        <thead>
-                            <tr className="bg-gray-200">
-                                <th className="px-4 py-2">كلمة المرور</th>
-                                <th className="px-4 py-2">البريد الإلكتروني</th>
-                                <th className="px-4 py-2">الاسم</th>
-                            </tr>
-                        </thead>
-                        <tbody className="text-gray-700">
-                            {employees.length > 0 ? (
-                                employees.map((employee) => (
-                                    <tr key={employee.id} className="border-t">
-                                        <td className="px-4 py-2">{employee.password}</td>
-                                        <td className="px-4 py-2">{employee.email}</td>
-                                        <td className="px-4 py-2"> {employee.lastname} {employee.firstname}</td>
-                                    </tr>
-                                ))
-                            ) : (
-                                <tr>
-                                    <td colSpan="3" className="px-4 py-2 text-center">لا يوجد مستخدمين</td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    );
->>>>>>> 2052b456d75835656b5b5701fca43ad98f03c651
 }
