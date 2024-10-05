45d7704237f054b93bee6bc03d51fcff9c801413
YoussefFathy6 <yousseffathy20@gmail.com>
2024-10-06 01:27:02 +0300

    emp

--- src/pages/Users/Employee/UserInfo.jsx
+++ src/pages/Users/Employee/UserInfo.jsx
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Card } from "flowbite-react";
-import { getFirestore, doc, getDoc, query, where, getDocs, collection } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  query,
+  where,
+  getDocs,
+  collection,
+} from "firebase/firestore";
 import Topbanner from "../../Home/componants/banner/Topbanner";
 import Bottombanner from "../../Home/componants/banner/Bottombanner";
 import { useTranslation } from "react-i18next";
@@ -16,6 +24,7 @@ export default function UserInfo() {
   const { t, i18n } = useTranslation("global");
 
   const direction = i18n.language === "ar" ? "rtl" : "ltr";
+  console.log(user);
 
   useEffect(() => {
     const fetchProxyEmployees = async () => {
@@ -45,12 +54,12 @@ export default function UserInfo() {
 
   useEffect(() => {
     const fetchSubjectByEmployeeID = async () => {
-      const subjectRef = collection(db, 'subjects');
-      const q = query(subjectRef, where('emp1.id', '==', user.id));
+      const subjectRef = collection(db, "subjects");
+      const q = query(subjectRef, where("emp1.id", "==", user.id));
       const subjectSnapshot = await getDocs(q);
       const subjects = subjectSnapshot.docs.map((doc) => doc.data());
       setEmpSubjects(subjects);
-    }
+    };
     fetchSubjectByEmployeeID();
   }, [user.id]);
 
@@ -59,84 +68,112 @@ export default function UserInfo() {
   }, [empSubjects]);
 
   const handleCardClick = (proxyEmployee) => {
-    navigate("/userProxy", { state: { user: proxyEmployee, mainUser: user.id } });
+    navigate("/userProxy", {
+      state: { user: proxyEmployee, mainUser: user.id },
+    });
   };
 
   return (
-    <div >
+    <div>
       <Topbanner />
-      <div className="min-h-screen bg-gray-100 justify-center flex items-center" dir={direction}>
+      <div
+        className="min-h-screen bg-gray-100 justify-center flex items-center"
+        dir={direction}
+      >
         <Card className="w-[900px] h-auto my-12">
           <div className="flex flex-col items-center pb-10">
             <img
               alt="User Avatar"
-              src={user.profileImage || "https://www.lightsong.net/wp-content/uploads/2020/12/blank-profile-circle.png"}
+              src={
+                user.profileImage ||
+                "https://www.lightsong.net/wp-content/uploads/2020/12/blank-profile-circle.png"
+              }
               className="mb-3 rounded-full shadow-lg w-60 h-60"
             />
             <div className="mt-4 w-full">
               <table className="min-w-full  border-collapse">
                 <tbody className="text-gray-700">
                   <tr>
-                  <td className="px-4 py-2 font-bold"> {t('userInfo.employeeName')}</td>
+                    <td className="px-4 py-2 font-bold">
+                      {" "}
+                      {t("userInfo.employeeName")}
+                    </td>
 
                     <td className="px-4 py-2">{user.employeeName}</td>
                   </tr>
                   <tr className="bg-gray-100">
-                  <td className="px-4 py-2 font-bold"> {t('userInfo.employeeId')}</td>
+                    <td className="px-4 py-2 font-bold">
+                      {" "}
+                      {t("userInfo.employeeId")}
+                    </td>
 
                     <td className="px-4 py-2">{user.employeeId}</td>
                   </tr>
                   <tr>
-                  <td className="px-4 py-2 font-bold">{t('userInfo.hireDate')}</td>
+                    <td className="px-4 py-2 font-bold">
+                      {t("userInfo.hireDate")}
+                    </td>
 
                     <td className="px-4 py-2">{user.hireDate}</td>
                   </tr>
                   <tr className="bg-gray-100">
-                  <td className="px-4 py-2 font-bold">{t('userInfo.jobGrade')}</td>
+                    <td className="px-4 py-2 font-bold">
+                      {t("userInfo.jobGrade")}
+                    </td>
 
                     <td className="px-4 py-2">{user.jobGrade}</td>
                   </tr>
                   <tr>
-                  <td className="px-4 py-2 font-bold">{t('userInfo.department')}</td>
+                    <td className="px-4 py-2 font-bold">
+                      {t("userInfo.department")}
+                    </td>
 
                     <td className="px-4 py-2">{user.department}</td>
                   </tr>
                   <tr className="bg-gray-100">
-                  <td className="px-4 py-2 font-bold">{t('userInfo.officeNumber')}</td>
+                    <td className="px-4 py-2 font-bold">
+                      {t("userInfo.officeNumber")}
+                    </td>
 
                     <td className="px-4 py-2">{user.officeNumber}</td>
                   </tr>
                   <tr>
-                  <td className="px-4 py-2 font-bold">{t('userInfo.jobTitle')}</td>
+                    <td className="px-4 py-2 font-bold">
+                      {t("userInfo.jobTitle")}
+                    </td>
 
                     <td className="px-4 py-2">{user.jobTitle}</td>
                   </tr>
                   <tr className="bg-gray-100">
-                  <td className="px-4 py-2 font-bold">{t('userInfo.phoneNumber')}</td>
+                    <td className="px-4 py-2 font-bold">
+                      {t("userInfo.phoneNumber")}
+                    </td>
 
                     <td className="px-4 py-2">{user.phoneNumber}</td>
                   </tr>
                   <tr className="">
-                  <td className="px-4 py-2 font-bold">{t('userInfo.email')}</td>
+                    <td className="px-4 py-2 font-bold">
+                      {t("userInfo.email")}
+                    </td>
 
                     <td className="px-4 py-2">{user.email}</td>
                   </tr>
                   <tr>
-                  <td className="px-4 py-2 font-bold">{t('userInfo.currentOffice')}</td>
+                    <td className="px-4 py-2 font-bold">
+                      {t("userInfo.currentOffice")}
+                    </td>
 
-                    <td className="px-4 py-2 break-words">{user.currentOffice}</td>
+                    <td className="px-4 py-2 break-words">
+                      {user.currentOffice}
+                    </td>
                   </tr>
                   <tr>
-
-                  <td className=" py-8 pt-10 font-bold">
-                    <h1 className="text-xl">{t('userInfo.proxyEmployeeTitle')}</h1>
-
+                    <td className=" py-8 pt-10 font-bold">
+                      <h1 className="text-xl">
+                        {t("userInfo.proxyEmployeeTitle")}
+                      </h1>
                     </td>
-                    <td>
-
-                    </td>
-
-                  
+                    <td></td>
                   </tr>
                   {/* عرض الموظفين البدلاء */}
                   {proxyEmployees.length > 0 ? (
@@ -146,37 +183,54 @@ export default function UserInfo() {
                           className={index % 2 === 0 ? "bg-gray-100" : ""}
                           onClick={() => handleCardClick(proxyEmployee)}
                         >
-                                                    <td className="px-4 py-2 font-bold"> {t('userInfo.proxyEmployeeName')}</td>
+                          <td className="px-4 py-2 font-bold">
+                            {" "}
+                            {t("userInfo.proxyEmployeeName")}
+                          </td>
 
-                          <td className="px-4 py-2">{proxyEmployee.proxyEmployeeName}</td>
+                          <td className="px-4 py-2">
+                            {proxyEmployee.proxyEmployeeName}
+                          </td>
                         </tr>
                         <tr
                           className={index % 2 === 0 ? "bg-gray-100" : ""}
                           onClick={() => handleCardClick(proxyEmployee)}
                         >
-                          <td className="px-4 py-2 font-bold">{t('userInfo.proxyPhoneNumber')}</td>
-                          <td className="px-4 py-2">{proxyEmployee.proxyPhoneNumber}</td>
-
+                          <td className="px-4 py-2 font-bold">
+                            {t("userInfo.proxyPhoneNumber")}
+                          </td>
+                          <td className="px-4 py-2">
+                            {proxyEmployee.proxyPhoneNumber}
+                          </td>
                         </tr>
                       </React.Fragment>
                     ))
                   ) : (
                     <tr>
                       <td className="px-4 py-2" colSpan="2">
-                        {t('userInfo.noProxyEmployees')}
+                        {t("userInfo.noProxyEmployees")}
                       </td>
                     </tr>
                   )}
-                   <td className="px-4 py-8 pt-10 font-bold">
-                    <h1 className="text-xl">{t('userInfo.permissionsTitle')}</h1>
+                  <td className="px-4 py-8 pt-10 font-bold">
+                    <h1 className="text-xl">
+                      {t("userInfo.permissionsTitle")}
+                    </h1>
                   </td>
                   <td></td>
-                 
+
                   {empSubjects.length > 0 ? (
                     empSubjects.map((subject) => (
-                      <tr key={subject.id}>
-                   
-                        <td className="px-4 py-2 font-bold">{t('userInfo.subjectTitle')}</td>
+                      <tr
+                        key={subject.id}
+                        className="cursor-pointer hover:bg-gray-100"
+                        onClick={() => {
+                          navigate("/subjectInfo", { state: { subject } });
+                        }}
+                      >
+                        <td className="px-4 py-2 font-bold ">
+                          {t("userInfo.subjectTitle")}
+                        </td>
                         <td className="px-4 py-2 break-words">
                           {subject.subjectTitle}
                         </td>
@@ -185,7 +239,7 @@ export default function UserInfo() {
                   ) : (
                     <tr>
                       <td colSpan={2} className="px-4 py-2 text-center">
-                        {t('userInfo.noRelatedSubjects')}
+                        {t("userInfo.noRelatedSubjects")}
                       </td>
                     </tr>
                   )}
--- src/pages/Users/Subjects/SubjectInfo.jsx
+++ src/pages/Users/Subjects/SubjectInfo.jsx
@@ -1,22 +1,21 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { Card } from "flowbite-react";
 import Topbanner from "../../Home/componants/banner/Topbanner";
 import Bottombanner from "../../Home/componants/banner/Bottombanner";
 import { useLocation, useNavigate } from "react-router-dom";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 import db from "../../../config/firebase";
-import { useTranslation } from "react-i18next";
 
 export default function SubjectInfo() {
   const location = useLocation();
   const navigate = useNavigate();
-  const { t, i18n } = useTranslation("global");
 
-  const direction = i18n.language === "ar" ? "rtl" : "ltr";
   const [subject, setSubject] = useState(null);
   const [employees, setEmployees] = useState([]);
   const clickedSubject = location.state?.subject;
 
+  // جلب بيانات المادة
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -25,6 +24,7 @@ export default function SubjectInfo() {
           return;
         }
 
+        // جلب بيانات الموظفين
         const employeesSnapshot = await getDocs(collection(db, "employees"));
         const employeesList = employeesSnapshot.docs.map((doc) => ({
           id: doc.id,
@@ -32,6 +32,8 @@ export default function SubjectInfo() {
         }));
 
         setEmployees(employeesList);
+
+        // هنا يمكنك القيام بأي جلب آخر للبيانات إذا كان لديك حقول إضافية
         setSubject(clickedSubject);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -42,9 +44,10 @@ export default function SubjectInfo() {
   }, [clickedSubject]);
 
   if (!subject || employees.length === 0) {
-    return <div>جاري التحميل...</div>;
+    return <div>جاري التحميل...</div>; // يمكنك وضع رسالة تحميل مؤقتة حتى جلب البيانات
   }
 
+  // العثور على الموظفين من مجموعة employees بناءً على الـ employeeId من subject
   const emp1 = employees.find(
     (emp) => emp.employeeId === clickedSubject.emp1Id
   );
@@ -55,76 +58,80 @@ export default function SubjectInfo() {
   return (
     <div>
       <Topbanner />
-      <div className="min-h-screen bg-gray-100 justify-center flex items-center" dir={direction}>
-        <Card className="w-[1200px]">
+      <div className="min-h-screen bg-gray-100 justify-center flex items-center">
+        <Card className="w-[1200px] ">
           <div className="flex justify-end px-4 pt-4"></div>
           <div className="flex flex-col items-center pb-10">
+            {/* الجدول */}
             <div className="mt-4 w-full">
-              <table className="min-w-full  border-collapse table-fixed">
+              <table className="min-w-full text-right border-collapse table-fixed">
                 <tbody className="text-gray-700">
                   <tr>
-                   
-                    <td className="px-4 py-2 font-bold w-1/2">
-                      {t("subjectInfo.subjectNumber")}
-                    </td>
                     <td className="px-4 py-2 break-words w-1/2">
                       {subject.subjectNum || "غير متاح"}
                     </td>
+                    <td className="px-4 py-2 font-bold w-1/2">: رقم المادة</td>
                   </tr>
                   <tr className="bg-gray-100">
-                    
-                    <td className="px-4 py-2 font-bold w-1/2">
-                      {t("subjectInfo.subjectTitle")}
-                    </td>
                     <td className="px-4 py-2 break-words w-1/2">
                       {subject.subjectTitle || "غير متاح"}
                     </td>
+                    <td className="px-4 py-2 font-bold w-1/2">
+                      : موضوع المادة
+                    </td>
                   </tr>
                   <tr>
-                   
-                    <td className="px-4 py-2 font-bold w-1/2">
-                      {t("subjectInfo.subjectContent")}
-                    </td>
                     <td className="px-4 py-2 break-words w-1/2 overflow-hidden">
                       {subject.subjectContent || "غير متاح"}
                     </td>
+                    <td className="px-4 py-2 font-bold w-1/2">: نص المادة</td>
                   </tr>
                   <tr className="bg-gray-100">
-                   
-                    <td className="px-4 py-2 font-bold w-1/2">
-                      {t("subjectInfo.authorizedEmployee")}
-                    </td>
                     <td className="px-4 py-2 break-words w-1/2">
-                      {subject.emp1?.employeeName || "غير متاح"} -{" "}
-                      {subject.emp1?.jobTitle || "غير متاح"}
+                      {subject.emp1.employeeName || "غير متاح"} -{" "}
+                      {subject.emp1.jobTitle || "غير متاح"}{" "}
+                    </td>
+                    <td className="px-4 py-2 font-bold w-1/2">
+                      : الموظف المفوض - المسمى الوظيفي
                     </td>
                   </tr>
                   <tr>
-                   
-                    <td className="px-4 py-2 font-bold w-1/2">
-                      {t("subjectInfo.sharedEmployees")}
+                    <td className="px-4 py-2 break-words w-1/2">
+                      {/* {emp2?.employeeName || "غير متاح"} -{" "}
+                      {emp2?.role || "غير متاح"} */}
                     </td>
-                 
+                    <td className="px-4 py-2 font-bold w-1/2">
+                      :الموظفين المشتركين - نوع الاشتراك
+                    </td>
                   </tr>
                   {subject.sharedEmployees.length > 0 ? (
                     subject.sharedEmployees.map((emp) => {
-                      const employee = employees.find(
+                      const user = employees.find(
                         (empl) => empl.employeeId === emp.empId
                       );
+                      console.log("Found user:", user); // Add this to see if it finds the employee
                       return (
                         <tr
                           className="cursor-pointer hover:bg-gray-100"
                           onClick={() => {
-                            navigate("/subjectInfo", { state: { subject } });
+                            if (user) {
+                              navigate("/userinfo", { state: { user } });
+                            } else {
+                              console.log(
+                                "No user found for empId:",
+                                emp.empId
+                              );
+                            }
                           }}
                           key={emp.empId}
                         >
-                        
-                          <td className="px-4 py-2 break-words w-1/2 overflow-hidden">
-                            {employee ? employee.employeeName : emp.empId}
-                          </td>
                           <td className="px-4 py-2 break-words w-1/2 overflow-hidden">
                             {emp.role}
+                          </td>
+                          <td className="px-4 py-2 break-words w-1/2 overflow-hidden">
+                            {user
+                              ? user.employeeName
+                              : `Employee ID: ${emp.empId}`}
                           </td>
                         </tr>
                       );
@@ -132,27 +139,23 @@ export default function SubjectInfo() {
                   ) : (
                     <tr>
                       <td colSpan={2} className="px-4 py-2 text-center">
-                        {t("subjectInfo.noRelatedSubjects")}
+                        لا توجد مواد مرتبطة
                       </td>
                     </tr>
                   )}
                   <tr className="bg-gray-100">
-                   
-                    <td className="px-4 py-2 font-bold w-1/2">
-                      {t("subjectInfo.negotiationLimit")}
-                    </td>
                     <td className="px-4 py-2 break-words w-1/2">
                       {subject.negotiationLimit}
                     </td>
+                    <td className="px-4 py-2 font-bold w-1/2">
+                      : حدود التفاوض
+                    </td>
                   </tr>
                   <tr>
-                   
-                    <td className="px-4 py-2 font-bold w-1/2">
-                      {t("subjectInfo.notes")}
-                    </td>
                     <td className="px-4 py-2 break-words w-1/2 overflow-hidden">
                       {subject.notes || "غير متاح"}
                     </td>
+                    <td className="px-4 py-2 font-bold w-1/2">: ملاحظات</td>
                   </tr>
                 </tbody>
               </table>
